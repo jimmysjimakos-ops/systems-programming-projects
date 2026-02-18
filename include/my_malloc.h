92b02f671d805d0f601199d5966f9bc0635de70c
jimmysjimakos-ops <jimmysjimakos.tsatsis@gmail.com>
2026-02-18 20:42:26 +0200

    Update my_malloc.h

--- include/my_malloc.h
+++ include/my_malloc.h
@@ -4,6 +4,6 @@
 #include <stddef.h>     //this for giving us size_t
 
 void *my_malloc(size_t size);   //this is my version of malloc which takes in a size_t size value
-void free(void *ptr);           //this is my version of free- takes a malloc ptr
+void my_free(void *ptr);           //this is my version of free- takes a malloc ptr
 
 #endif
