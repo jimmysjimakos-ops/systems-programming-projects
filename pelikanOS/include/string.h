8e9791c31cbdf7376e47ea233cd669ba22a32e54
jimmysjimakos-ops <jimmysjimakos.tsatsis@gmail.com>
2026-08-09 11:34:47 +0300

    PelikanOS: added print_hex and memcpy funcs

--- pelikanOS/include/string.h
+++ pelikanOS/include/string.h
@@ -4,5 +4,6 @@
 #include <stdint.h>
 
 void memset(void *buf, uint8_t val, uint32_t size);
+void memcpy(void *dest, const void *src, uint32_t size);
 
 #endif
--- pelikanOS/src/string.c
+++ pelikanOS/src/string.c
@@ -6,3 +6,49 @@ void memset(void *buf, uint8_t val, uint32_t size){
         p[i] = val;
     }
 }
+
+void memcpy(void *dest, const void *src, uint32_t size){
+    uint8_t *d = (uint8_t *)dest;
+    const uint8_t *s = (const uint8_t *)src;
+    for(uint32_t i = 0; i < size; i++){
+        d[i] = s[i];
+    }
+}
+
+/*
+
+uint8_t *print_hex(uint8_t *buf,uint32_t num){ //works for both byte AND uint
+    uint32_t nibble;
+    uint32_t and_thing = 0x0000000F;
+
+    for(int i=0; i<8; i++){
+        nibble = num & (and_thing << i * 4);
+        nibble = nibble >> i;
+        if(nibble <= 9){   //must not receive negative nums
+            buf[i] = '0' + nibble;
+        }else if(nibble > 9){
+            buf[i] = (nibble - 10) + 'A';
+        }
+        
+    }
+    buf[8] = '\0';
+
+    return buf;
+}
+    the issue is this prints it reversed , so ill have to reverse it , i can instead work top down
+*/
+
+uint8_t *print_hex(uint8_t *buf,uint32_t num){ //works for both byte AND uint
+    uint32_t nibble;
+    for(int i = 7; i > -1; i--){
+        nibble = (num >> (i * 4)) & 0x0F;
+         if(nibble <= 9){   //must not receive negative nums
+            buf[7-i] = '0' + nibble;
+        }else if(nibble > 9){
+            buf[7-i] = (nibble - 10) + 'A';
+        }
+    }
+    buf[8] = '\0';
+
+    return buf;
+}
